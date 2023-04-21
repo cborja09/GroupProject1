8c87a3d67951d55db053f3628d3295a1acf35620
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 11:58:46 -0700

    Updated Commit

--- src/Number1.java
+++ src/Number1.java
@@ -6,6 +6,23 @@ public class Number1 {
         to find the highest and lowest temperature for the week.
          */
 
+        int [] temperature= {45,65,70,80,85,99,109};
+        int high= temperature[0];
+        int low= temperature[0];
+
+        for (int i = 0; i < temperature.length ; i++) {
+            if (temperature[i] > high){
+                high=temperature[i];
+            } else if (temperature[i] < low) {
+                low=temperature[i];
+            }
+        }
+        System.out.println("The highest temperature of the week is " +high);
+        System.out.println("The lowest temperature of the week is " +low);
+
+
+
+
 
     }
 }
