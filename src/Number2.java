9ed68e3f8bcbc4f06a302968f6a4debd3bd76d23
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 12:05:57 -0700

    Updated Commit

--- src/Number2.java
+++ src/Number2.java
@@ -5,5 +5,14 @@ public class Number2 {
         Create an array of integer values. After the array is created, calculate the sum of all
         stored elements in that array.
          */
+
+        int[] numbers= {10,20,30,85};
+
+        int sum=0;
+
+        for (int i= 0; i < 4; i++) {
+            sum+=numbers[i];
+        }
+        System.out.println(sum);
     }
 }
