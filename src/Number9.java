3fc576945d978e30e256539db66217842844d932
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 14:20:25 -0700

    Updated Commit

--- src/Number9.java
+++ src/Number9.java
@@ -3,7 +3,19 @@ public class Number9 {
 
         // Write a java program to find the second-largest number in the array?
 
+        int[] array= {1,2,3,4,5,6,7,8,9};
 
+        int largest= array[0];
+        int secondLargest= array[0];
 
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largest){
+                secondLargest= largest;
+                largest= array[i];
+            } else if (array[i] > secondLargest && array[i] != largest) {
+                secondLargest= array[i];
+            }
+        }
+        System.out.println("The second largest number is " + secondLargest);
     }
 }
