38ff9be96be7e5910bf1a99ba5618e3f0dfd7fc5
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 14:29:35 -0700

    Updated Commit

--- src/Number10.java
+++ src/Number10.java
@@ -3,6 +3,14 @@ public class Number10 {
 
         // Write a program to print out duplicate elements from an Array of Strings?
 
+        String[] cars= {"Subaru","BMW","Toyota","Rivian","Subaru"};
 
+        for (int i = 0; i < cars.length; i++) {
+            for (int j = 0; j < cars.length; j++) {
+                if(cars[i].equals(cars[j])){
+                    System.out.println("Duplicate car is " + cars[j]);
+                }
+            }
+        }
     }
 }
