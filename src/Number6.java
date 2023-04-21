0570481847982374414f2204976f4f358a148b3c
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 13:46:47 -0700

    Updated Commit

--- src/Number6.java
+++ src/Number6.java
@@ -3,6 +3,18 @@ public class Number6 {
 
         // Write a java program to check whether a given number is prime or not?
 
-
+        int number=19;
+        boolean prime=true;
+        for (int i = 2; i < number; i++) {
+            if(number%i==0){
+                prime=false;
+                break;
+            }
+        }
+        if (prime){
+            System.out.println("The number "+number+" is prime");
+        }else {
+            System.out.println("The number "+number+" is prime");
+        }
     }
 }
