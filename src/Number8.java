b27d7b88dc358f1881d7be15afbdb47e3f24ffa4
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 14:12:08 -0700

    Updated Commit

--- src/Number8.java
+++ src/Number8.java
@@ -3,6 +3,19 @@ public class Number8 {
 
         // Maximum and minimum number in the array?
 
+        int[] array= {2,4,6,8,10};
 
+        int max= array[0];
+        int min= array[0];
+
+        for (int i = 2; i < array.length; i++) {
+            if (array[i]>max){
+                max= array[i];
+            } else if (array[i]<min) {
+                min= array[i];
+            }
+        }
+        System.out.println("Maximum number is " + max);
+        System.out.println("Minimum number is " + min);
     }
 }
