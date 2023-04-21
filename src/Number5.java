0847e062b31be9b600b89f770ee38a88e7a180b2
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 12:52:54 -0700

    Updated Commit

--- src/Number5.java
+++ src/Number5.java
@@ -3,6 +3,17 @@ public class Number5 {
 
         //  Write a program to swap 2 numbers without a temporary variable?
 
+        int a= 10;
+        int b= 30;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(" After the swap : ");
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+
 
     }
 }
