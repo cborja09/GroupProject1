3eff4b55026c456a8c0cf32f1ca06cd8e19a4800
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 12:41:34 -0700

    Updated Commit

--- src/Number4.java
+++ src/Number4.java
@@ -6,8 +6,26 @@ public class Number4 {
         even and odd numbers for that array.
          */
 
+        int[][] numbers= {
+                {1,3,5,7,9,11},
+                {12,14,16,18,20,22},
 
+        };
 
+        int sumEven=0;
+        int sumOdd=0;
 
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                if(numbers[i][j]%2==0){
+                    sumEven+= numbers[i][j];
+                } else if (numbers[i][j]%2!=0) {
+                    sumOdd+= numbers[i][j];
+                }
+            }
+            
+        }
+        System.out.println(sumEven);
+        System.out.println(sumOdd);
     }
 }
