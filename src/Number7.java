c2ed5b9bbb922a07bbb161c21ed0e3b3c02ca5e1
cborja09@icloud.com <cborja09@icloud.com>
2023-04-21 13:59:45 -0700

    Updated Commit

--- src/Number7.java
+++ src/Number7.java
@@ -3,6 +3,18 @@ public class Number7 {
 
         // Write a Java Program to print the first 10 numbers of Fibonacci series.
 
+        int a= 0;
+        int b= 1;
+        int c;
 
+        a = 0;
+        b = 1;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a +" ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
     }
 }
